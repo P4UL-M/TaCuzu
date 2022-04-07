a57e6afe5e7114b90f854a870062b22c97891646
P4UL-M <paul.mairesse@free.fr>
2022-04-08 00:39:34 +0200

    remove print

--- data.c
+++ data.c
@@ -12,21 +12,18 @@ int *createArray(int n)
 void modifyValue(int *a, index i, bool val)
 {
     bool _val = getValue(a, i);
-    printf("val : %d\n", a[i.y]);
     if (val == _val)
     {
         return;
     }
     else if (val)
     {
-        printf("adding %d\n", 1 << i.x);
         a[i.y] += 1 << i.x;
     }
     else
     {
         a[i.y] -= 1 << i.x;
     }
-    printf("val : %d\n", a[i.y]);
 }
 
 bool getValue(int *a, index i)
